520ab1eaf7bddecc11e909569f14e0d18d411753
sonhyeryeong <104577534+sonhyeryeong@users.noreply.github.com>
2022-05-09 15:20:04 +0900

    Add files via upload

---
+++ trn02/js/main.js
@@ -0,0 +1,32 @@
+$(function () {
+    //////////////////////////////////////////////////
+
+
+    $('.top_banner i').on('click', function () {
+        $('.top_banner').slideUp(5000);
+    });
+
+
+
+     $('.main_slider').on('init reIint afterChange', function () {
+        let current = $('.slick-current');
+         current.addClass('on').siblings().removeClass('on');
+        });
+
+    //  $('.main_slider').on('init reIint afterChange', function () {
+    //     let current = $('.slick-current');
+    //     current.toggleClass('on')});
+
+
+    $('.main_slider').slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+    });
+
+
+
+    ///////////////////////////////////////////////////
+})
