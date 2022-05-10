29beafc6ebaeabcc811646739cb631d613a1f984
sonhyeryeong <104577534+sonhyeryeong@users.noreply.github.com>
2022-05-10 14:14:30 +0900

    Update main.js

--- trn02/js/main.js
+++ trn02/js/main.js
@@ -8,10 +8,10 @@ $(function () {
 
 
 
-     $('.main_slider').on('init reIint afterChange', function () {
+    $('.main_slider').on('init reIint afterChange', function () {
         let current = $('.slick-current');
-         current.addClass('on').siblings().removeClass('on');
-        });
+        current.addClass('on').siblings().removeClass('on');
+    });
 
     //  $('.main_slider').on('init reIint afterChange', function () {
     //     let current = $('.slick-current');
@@ -24,6 +24,33 @@ $(function () {
         autoplay: true,
         pauseOnHover: false,
         pauseOnFocus: false,
+    });
+
+    $('.movie .dec i:nth-of-type(1)').on('click', function () {
+        $('.movie video').trigger('play');
+    });
+
+    $('.movie .dec i:nth-of-type(2)').on('click', function () {
+        $('.movie video').trigger('pause');
+    });
+
+    $('#myMovie').YTPlayer({
+        videoURL: 'https://youtu.be/juhscgB-RSE',
+        containment: '.utube',
+        autoPlay: true,
+        mute: false,
+        startAt: 0,
+        opacity: 1,
+        showControls: false,
+        playOnlyIfVisible: true,
+    });
+
+    $('.utube i:nth-of-type(1)').on('click', function () {
+        $('#myMovie').YTPPlay();
+    });
+
+    $('.utube i:nth-of-type(2)').on('click', function () {
+        $('#myMovie').YTPPause();
     });
 
 
